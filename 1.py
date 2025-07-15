02660d5545a24df9993328474fd25b6e8d1a726f
MythriNagaraj <mythrigirija@gmail.com>
2025-07-15 11:55:26 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+#Ask the user to enter two numbers and print their sum.
+Num1=int(input("Ënter the first number: "))
+Num2=int(input("Enter the second number: "))
+
+Sum = Num1 + Num2
+
+print("The sum of two number is",Sum)
---
+++ 2.py
@@ -0,0 +1,11 @@
+#string manipulations
+name = " mythri loves python "
+print(name.upper()) #uppercase
+print(name.lower()) #lowercase
+print(name * 5) # repeating multiple times
+print(name.replace("mythri","mahima")) # to replace the string elements
+print(name[::2]) #split method
+print(name[::])# to show full string
+print(name[:-1])# it shows ulta
+print("mythri \t loves python")
+print("mythri \n loves python")
